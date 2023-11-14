80d12b8d7e5abba3eceae12b9d5c9eff4f770261
sksatyam <killersksatyam@gmail.com>
2023-11-14 23:02:17 +0530

    Hello world program

---
+++ helloworld.kt
@@ -0,0 +1,7 @@
+fun main(){
+    /*This is the Hello world program in Kotlin 
+    Kotlin compiler allways starts reading code from the main function
+    and in  kotlin we can create function using 'fun' keyword
+    we doesn't need to use semicolumn in the end of the line like python*/
+    println("Hello World")
+}
